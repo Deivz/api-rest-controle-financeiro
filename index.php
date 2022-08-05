1208febec78911b401cfd3b63b5f81fa6e8cf266
Deivz <carlosd_oliveira@hotmail.com>
2022-08-05 01:04:58 -0300

    Testando index na root

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require __DIR__ . '/../vendor/autoload.php';
 set_error_handler("Deivz\ApiRestControleFinanceiro\helpers\ErrorHandler::handleError");
 set_exception_handler("Deivz\ApiRestControleFinanceiro\helpers\ErrorHandler::handleException");
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 header('Content-type: application/json; charset = UTF-8');
@@ -18,7 +18,7 @@ $partes = explode('/', $_SERVER['REQUEST_URI']);
 $rota = $partes[1];
 $id = $partes[2] ?? null;
 
-$rotas = require __DIR__ . '/../config/routes.php';
+$rotas = require __DIR__ . '/config/routes.php';
 
 if (!array_key_exists($rota, $rotas)) {
    http_response_code(404);
